7e81f2072474d7e2c2e1720d6af6b78721fdeded
facenord <facenord.sud@gmail.com>
2015-04-01 13:59:31 +0200

    added comments

--- src/main/java/test/tomcat/integration/TomactIntegration.java
+++ src/main/java/test/tomcat/integration/TomactIntegration.java
@@ -13,7 +13,11 @@ import org.apache.tomcat.util.net.AbstractEndpoint;
 import org.apache.tomcat.util.net.Nio2Channel;
 
 /**
- *
+ * A demonstration of how to create a new protocol.
+ * To use it:
+ * 1. run ant build and copy the generated JAR to the lib path of the tomcat build.
+ * 2. Modify the file conf/server.xml to use this new "protocol" (test.tomcat.integration.TomactIntegration)
+ * 3. start Tomcat and search Hello world! through the logs
  * @author leo
  */
 public class TomactIntegration extends Http11Nio2Protocol{
