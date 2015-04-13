b2dd6510cc1ccb30e974fcd360d4006a8324a7db
facenord <facenord.sud@gmail.com>
2015-04-13 13:13:23 +0200

    a more firendly build.properties

--- src/main/java/ch/uninbf/mcs/tomcatopenssl/net/ssl/open/OpenSSLContext.java
+++ src/main/java/ch/uninbf/mcs/tomcatopenssl/net/ssl/open/OpenSSLContext.java
@@ -7,9 +7,19 @@ package ch.uninbf.mcs.tomcatopenssl.net.ssl.open;
 
 import io.netty.handler.ssl.CipherSuiteConverter;
 import io.netty.handler.ssl.OpenSsl;
+import java.io.IOException;
+import java.security.KeyFactory;
 import java.security.KeyManagementException;
+import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.SecureRandom;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -23,8 +33,11 @@ import javax.net.ssl.SSLParameters;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSessionContext;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import org.apache.tomcat.jni.CertificateVerifier;
 import org.apache.tomcat.jni.Pool;
 import org.apache.tomcat.jni.SSL;
 import org.apache.tomcat.jni.SSLContext;
@@ -203,8 +216,71 @@ public class OpenSSLContext extends SslContext {
         synchronized (OpenSSLContext.class) {
             try {
                 init();
+                    KeyStore ks = KeyStore.getInstance("JKS");
+                    ks.load(null, null);
+                    CertificateFactory cf = CertificateFactory.getInstance("X.509");
+                    KeyFactory rsaKF = KeyFactory.getInstance("RSA");
+                    KeyFactory dsaKF = KeyFactory.getInstance("DSA");
+
+                    ByteBuf encodedKeyBuf = PemReader.readPrivateKey(keyFile);
+                    byte[] encodedKey = new byte[encodedKeyBuf.readableBytes()];
+                    encodedKeyBuf.readBytes(encodedKey).release();
+
+                    char[] keyPasswordChars = keyPassword.toCharArray();
+                    PKCS8EncodedKeySpec encodedKeySpec = generateKeySpec(keyPasswordChars, encodedKey);
+
+                    PrivateKey key;
+                    try {
+                        key = rsaKF.generatePrivate(encodedKeySpec);
+                    } catch (InvalidKeySpecException ignore) {
+                        key = dsaKF.generatePrivate(encodedKeySpec);
+                    }
+
+                    List<Certificate> certChain = new ArrayList<Certificate>();
+                    ByteBuf[] certs = PemReader.readCertificates(certChainFile);
+                    try {
+                        for (ByteBuf buf: certs) {
+                            certChain.add(cf.generateCertificate(new ByteBufInputStream(buf)));
+                        }
+                    } finally {
+                        for (ByteBuf buf: certs) {
+                            buf.release();
+                        }
+                    }
+
+                    ks.setKeyEntry("key", key, keyPasswordChars, certChain.toArray(new Certificate[certChain.size()]));
+
+                    if (trustManagerFactory == null) {
+                        // Mimic the way SSLContext.getInstance(KeyManager[], null, null) works
+                        trustManagerFactory = TrustManagerFactory.getInstance(
+                                TrustManagerFactory.getDefaultAlgorithm());
+                        trustManagerFactory.init((KeyStore) null);
+                    } else {
+                        trustManagerFactory.init(ks);
+                    }
+
+                    final X509TrustManager manager = chooseTrustManager(trustManagerFactory.getTrustManagers());
+                    SSLContext.setCertVerifyCallback(ctx, new CertificateVerifier() {
+                        @Override
+                        public boolean verify(long ssl, byte[][] chain, String auth) {
+                            X509Certificate[] peerCerts = certificates(chain);
+                            try {
+                                manager.checkClientTrusted(peerCerts, auth);
+                                return true;
+                            } catch (Exception e) {
+                                logger.debug("verification of certificate failed", e);
+                            }
+                            return false;
+                        }
+                    });
             } catch (SSLException ex) {
                 //TODO: catch exception
+            } catch (IOException ex) {
+                Logger.getLogger(OpenSSLContext.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(OpenSSLContext.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (CertificateException ex) {
+                Logger.getLogger(OpenSSLContext.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
--- src/main/java/ch/uninbf/mcs/tomcatopenssl/net/ssl/open/OpenSSLSocketFactory.java
+++ src/main/java/ch/uninbf/mcs/tomcatopenssl/net/ssl/open/OpenSSLSocketFactory.java
@@ -73,6 +73,18 @@ public class OpenSSLSocketFactory implements SSLUtil, ServerSocketFactory{
     public KeyManager[] getKeyManagers() throws Exception {
         KeyManager[] managers = { new OpenSSLKeyManager(getEndpoint().getCertChainFile(), getEndpoint().getKeyFile()) };
         return managers;
+//        String keystoreType = endpoint.getKeystoreType();
+//        if (keystoreType == null) {
+//            keystoreType = defaultKeystoreType;
+//        }
+//
+//        String algorithm = endpoint.getAlgorithm();
+//        if (algorithm == null) {
+//            algorithm = KeyManagerFactory.getDefaultAlgorithm();
+//        }
+//
+//        return getKeyManagers(keystoreType, endpoint.getKeystoreProvider(),
+//                algorithm, endpoint.getKeyAlias());
     }
     
     private void checkFileExists(File file) throws IOException {
@@ -81,6 +93,125 @@ public class OpenSSLSocketFactory implements SSLUtil, ServerSocketFactory{
             throw new FileNotFoundException();
         }
     }
+    
+//    /**
+//     * Gets the initialized key managers.
+//     */
+//    private KeyManager[] getKeyManagers(String keystoreType,
+//                                          String keystoreProvider,
+//                                          String algorithm,
+//                                          String keyAlias)
+//                throws Exception {
+//
+//        KeyManager[] kms = null;
+//
+//        String keystorePass = getKeystorePassword();
+//
+//        KeyStore ks = getKeystore(keystoreType, keystoreProvider, keystorePass);
+//        if (keyAlias != null && !ks.isKeyEntry(keyAlias)) {
+//            throw new IOException(
+//                    sm.getString("jsse.alias_no_key_entry", keyAlias));
+//        }
+//
+//        KeyManagerFactory kmf = KeyManagerFactory.getInstance(algorithm);
+//        String keyPass = endpoint.getKeyPass();
+//        if (keyPass == null) {
+//            keyPass = keystorePass;
+//        }
+//        kmf.init(ks, keyPass.toCharArray());
+//
+//        kms = kmf.getKeyManagers();
+//        if (keyAlias != null) {
+//            String alias = keyAlias;
+//            if ("JKS".equals(keystoreType)) {
+//                alias = alias.toLowerCase(Locale.ENGLISH);
+//            }
+//            for(int i=0; i<kms.length; i++) {
+//                kms[i] = new JSSEKeyManager((X509KeyManager)kms[i], alias);
+//            }
+//        }
+//
+//        return kms;
+//    }
+//    
+//    /*
+//     * Gets the SSL server's keystore password.
+//     */
+//    protected String getKeystorePassword() {
+//        String keystorePass = endpoint.getKeystorePass();
+//        if (keystorePass == null) {
+//            keystorePass = endpoint.getKeyPass();
+//        }
+//        if (keystorePass == null) {
+//            keystorePass = DEFAULT_KEY_PASS;
+//        }
+//        return keystorePass;
+//    }
+//    
+//    /*
+//     * Gets the SSL server's keystore.
+//     */
+//    protected KeyStore getKeystore(String type, String provider, String pass)
+//            throws IOException {
+//
+//        String keystoreFile = endpoint.getKeystoreFile();
+//        if (keystoreFile == null)
+//            keystoreFile = defaultKeystoreFile;
+//
+//        return getStore(type, provider, keystoreFile, pass);
+//    }
+//    
+//    private KeyStore getStore(String type, String provider, String path,
+//            String pass) throws IOException {
+//
+//        KeyStore ks = null;
+//        InputStream istream = null;
+//        try {
+//            if (provider == null) {
+//                ks = KeyStore.getInstance(type);
+//            } else {
+//                ks = KeyStore.getInstance(type, provider);
+//            }
+//            if(!("PKCS11".equalsIgnoreCase(type) ||
+//                    "".equalsIgnoreCase(path))) {
+//                File keyStoreFile = new File(path);
+//                if (!keyStoreFile.isAbsolute()) {
+//                    keyStoreFile = new File(System.getProperty(
+//                            Constants.CATALINA_BASE_PROP), path);
+//                }
+//                istream = new FileInputStream(keyStoreFile);
+//            }
+//
+//            char[] storePass = null;
+//            if (pass != null && !"".equals(pass)) {
+//                storePass = pass.toCharArray();
+//            }
+//            ks.load(istream, storePass);
+//        } catch (FileNotFoundException fnfe) {
+//            log.error(sm.getString("jsse.keystore_load_failed", type, path,
+//                    fnfe.getMessage()), fnfe);
+//            throw fnfe;
+//        } catch (IOException ioe) {
+//            // May be expected when working with a trust store
+//            // Re-throw. Caller will catch and log as required
+//            throw ioe;
+//        } catch(Exception ex) {
+//            String msg = sm.getString("jsse.keystore_load_failed", type, path,
+//                    ex.getMessage());
+//            log.error(msg, ex);
+//            throw new IOException(msg);
+//        } finally {
+//            if (istream != null) {
+//                try {
+//                    istream.close();
+//                } catch (IOException ioe) {
+//                    // Do nothing
+//                }
+//            }
+//        }
+//
+//        return ks;
+//    }
 
     @Override
     public TrustManager[] getTrustManagers() throws Exception {
